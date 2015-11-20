77c92ba82fe3f5c5c1110a948f57fac8c6b4e19a
lilkr <maintiendrai@gmail.com>
2015-11-20 16:13:13 +0800

    remove plot

--- JEHighchartsDigesterDemo/JEHighchartsDigesterDemo/JEHighchartsDigester/js/highcharts/viewOption/donutLegend.js
+++ JEHighchartsDigesterDemo/JEHighchartsDigesterDemo/JEHighchartsDigester/js/highcharts/viewOption/donutLegend.js
@@ -1,129 +1,157 @@
 $(function () {
-  
-  $(document).ready(function () {
-                    
-                    // Build the chart
-                    $('#container').highcharts({
-                                               chart: {
-//                                               height: 480,
-//                                               width: 320
-                                               
-                                               },
-                                               yAxis: [
-                                                       
-                                                       { // Primary yAxis
-                                                       title: {
-                                                       text: ''
-                                                       },
-                                                       min: 0
-                                                       }, { // Secondary yAxis
-                                                       top: 220,
-                                                       // min: 0,
-                                                       height: 140,
-                                                       bottom: 100,
-                                                       // max: 100,
-                                                       title: {
-                                                       text: '',
-                                                       style: {
-                                                       color: '#4572A7'
-                                                       }
-                                                       }
-                                                       //   ,
-                                                       //   labels: {
-                                                       //       style: {
-                                                       //           color: '#4572A7'
-                                                       //       }
-                                                       //   },
-                                                       //   opposite: true
-                                                       }],
-                                               
-                                                   title: {
-                                                       text: ''
-                                                   },
-                                                   tooltip: {
-                                                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                                                   },
-                                                   plotOptions: {
-                                                       pie: {
-                                                       allowPointSelect: true,
-                                                       cursor: 'pointer',
-                                                       dataLabels: {
-                                                       enabled: false
-                                                       },
-                                                       showInLegend: true,
-                                                           point:{
-                                                           events : {
-                                                           legendItemClick: function(e){
-                                                           e.preventDefault();
-                                                           }
-                                                           }
-                                                           }
-                                                   },
-                                               
-                                                   area: {
-                                                       width: 10,
-                                                       height: 10,
-                                                       depth: 10,
-                                                       showInLegend: false
-                                                   }
-                                               
-                                               },
-                                               
-                                               legend: {
-                                                   floating: true,
-                                                   align: 'left',
-                                                   x: 10, // = marginLeft - default spacingLeft
-                                                   itemWidth: 160,
-                                                   verticalAlign: 'bottom',
-                                               },
-                                               
-                                               series: [{
-                                                        type: 'area',
-                                                        name: 'Jane',
-                                                        top: 400,
-//                                                        lineColor: '#666666',
-                                                        // cropThreshold: 100,
-                                                        yAxis: 1,
-                                                        data: [3, 2, 1, 3, 4]
-                                                        },
-                                                        
-                                                        
-                                                        {
-                                                        type: 'pie',
-                                                        options3d: {
-                                                        enabled: false,
-                                                        alpha: 45
-                                                        },
-                                                     //   align: 'center',
-                                                        y:-100,
-                                                        verticalAlign: 'top',
-                                                        name: 'Brands',
-                                                        colorByPoint: true,
-                                                        center: [120, 80],
-                                                        size: 180,
-                                                        innerSize: 90,
-                                                        data: [{
-                                                               name: 'Microsoft Internet Explorer',
-                                                               y: 56.33
-                                                               }, {
-                                                               name: 'Chrome',
-                                                               y: 24.03,
-                                                               sliced: true,
-                                                               selected: true
-                                                               }, {
-                                                               name: 'Firefox',
-                                                               y: 10.38
-                                                               }, {
-                                                               name: 'Safari',
-                                                               y: 4.77
-                                                               }, {
-                                                               name: 'Opera',
-                                                               y: 0.91
-                                                               }, {
-                                                               name: 'Proprietary or Undetectable',
-                                                               y: 0.2
-                                                               }]
-                                                        }]
-                                               });
-                    });
-  });
+
+    $(document).ready(function () {
+
+        // Build the chart
+        $('#container').highcharts({
+            chart: {
+                //                                               height: 480,
+                //                                               width: 320
+
+            },
+           xAxis: [{ // Primary xAxis
+                   title: {
+                   text: '整数',
+                   //  align: 'center',
+                   // center: [120, 80],
+                   x: 160,
+                   y: 90,
+                   
+                   //  verticalAlign: 'middle'
+                   },
+                   width: 0,
+                   height:0,
+                   min: 0
+                   },
+                   {
+                   height:390   //写死了
+                   }],
+            yAxis: [
+
+            { // Primary yAxis
+                title: {
+                    text: ''
+                },
+                min: 0
+            }, { // Secondary yAxis
+                top: 280,
+                // min: 0,
+                height: 140,   //写死了
+//                bottom: 100,
+                // max: 100,
+                title: {
+                    text: '',
+                    style: {
+                        color: '#4572A7'
+                    }
+                }
+                //   ,
+                //   labels: {
+                //       style: {
+                //           color: '#4572A7'
+                //       }
+                //   },
+                //   opposite: true
+            }],
+
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true,
+                    point: {
+                        events: {
+                            legendItemClick: function (e) {
+                                e.preventDefault();
+                            }
+                        }
+                    }
+                },
+
+                area: {
+                    width: 10,
+                    height: 10,
+                    depth: 10,
+                    showInLegend: false,
+                                   marker: {
+                                   enabled: false,
+                                   symbol: 'circle',
+                                   radius: 2,
+                                   states: {
+                                   hover: {
+                                   enabled: true
+                                   }
+                                   }
+                                   }
+                }
+
+            },
+
+            legend: {
+                floating: true,
+                align: 'left',
+                x: 10, // = marginLeft - default spacingLeft
+                itemWidth: 160,
+                verticalAlign: 'bottom',
+            },
+
+            series: [{
+                type: 'area',
+                name: 'Jane',
+                top: 400,
+                //                                                        lineColor: '#666666',
+                // cropThreshold: 100,
+                yAxis: 1,
+                xAxis:1,
+                data: [3, 2, 1, 3, 4]
+            },
+
+
+            {
+                type: 'pie',
+                options3d: {
+                    enabled: false,
+                    alpha: 45
+                },
+                //   align: 'center',
+                y: -100,
+                verticalAlign: 'top',
+                name: 'Brands',
+                colorByPoint: true,
+                center: [140, 80], //写死了
+                size: 180,
+                innerSize: 90,
+                data: [{
+                    name: 'Microsoft Internet Explorer',
+                    y: 56.33
+                }, {
+                    name: 'Chrome',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Firefox',
+                    y: 10.38
+                }, {
+                    name: 'Safari',
+                    y: 4.77
+                }, {
+                    name: 'Opera',
+                    y: 0.91
+                }, {
+                    name: 'Proprietary or Undetectable',
+                    y: 0.2
+                }]
+            }]
+        });
+    });
+});
