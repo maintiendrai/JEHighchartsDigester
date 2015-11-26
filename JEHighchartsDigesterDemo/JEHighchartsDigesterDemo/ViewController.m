3bd635c1b5f37765a5ff610471fbca56a5289f79
lilkr <maintiendrai@gmail.com>
2015-11-26 10:52:57 +0800

    run javascript version

--- JEHighchartsDigesterDemo/JEHighchartsDigesterDemo/ViewController.m
+++ JEHighchartsDigesterDemo/JEHighchartsDigesterDemo/ViewController.m
@@ -33,44 +33,37 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self htmlDigester];
     
-    // Option 1: working
-     NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"donutLegend" ofType:@"html"];
+}
+
+
+//method 1, 直接操作html
+- (void)htmlDigester {
+
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"donutLegend" ofType:@"html"];
     _htmlString  = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
-     NSURL *baseURL = [NSURL fileURLWithPath:htmlFile];
-     
-     [self.webView loadHTMLString:_htmlString baseURL:baseURL];
+    NSURL *baseURL = [NSURL fileURLWithPath:htmlFile];
+    
+    [self.webView loadHTMLString:_htmlString baseURL:baseURL];
+    self.webView.delegate = self;
+}
+
+//method 2, 直接操作js，html采用拼接
+- (void)jsDigester {
     
     self.webView.delegate = self;
     
-    
-    
-    
-    return;
     NSString* format = EMPTY_WEB_VIEW;
     NSString* html = [NSString stringWithFormat:format,
                       self.view.bounds.size.width,
                       CGRectGetHeight(self.webView.frame),
                       CGRectGetHeight(self.webView.frame)/2.f];
     
-    
-    
-    NSBundle *thisBundle = [NSBundle mainBundle];
-    NSString *path = [thisBundle pathForResource:@"donutLegend" ofType:@"html"];
-//    NSURL *instructionsURL = [NSURL fileURLWithPath:path];
-
-    
-    [self.webView loadHTMLString:path baseURL:nil];
-    
-    
-    
-//    self.donutWebView.delegate = self;
-//    [self.donutWebView loadHTMLString:html baseURL:nil];
-//    
-//    [self.donutWebView setBackgroundColor:[UIColor blueColor]];
-    
-    
+    [self.webView loadHTMLString:html baseURL:nil];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -96,18 +89,20 @@
 //    [context evaluateScript:@"function greet(name){ return 'Hello, ' + name; }"];
     
 //    [context evaluateScript:@"function plots(data) {var chart = $('#container').highcharts();inCome = [0, 1, 2];chart.series[0].setData(inCome, true);  return 'Hello, ' + name; }"];
-//    [context evaluateScript: self.javascriptText.text];
+
+    
     JSValue *function = context[@"greet"];
     JSValue* result = [function callWithArguments:@[@"World"]];
     
-    NSLog(@"======================= %@", [result toString]);
+//    NSLog(@"======================= %@", [result toString]);
     context[@"hello"] = ^(NSString *text) {
         NSLog(@"JS : %@",text);
     };
 }
 
 
-- (void)loadWeb {
+//use when method2
+- (void)loadJSLibFiles {
     //load jquery.js
     NSString* jqueryPath = [[NSBundle mainBundle] pathForResource:@"jquery-1.9.0" ofType:@"js"];
     NSString* jquery = [NSString stringWithContentsOfFile:jqueryPath encoding:NSUTF8StringEncoding error:nil];
@@ -118,68 +113,82 @@
     NSString* highchart = [NSString stringWithContentsOfFile:highChartPath encoding:NSUTF8StringEncoding error:nil];
     [self.webView stringByEvaluatingJavaScriptFromString:highchart];
     
-    //    //load viewOption
-    //    NSString* baselinePath = [[NSBundle mainBundle] pathForResource:@"baseline" ofType:@"js"];
-    //    NSString* baselineInit = [NSString stringWithContentsOfFile:baselinePath encoding:NSUTF8StringEncoding error:nil];
-    //
     
     //load viewOption
     //    NSString* donutPath = [[NSBundle mainBundle] pathForResource:@"donut" ofType:@"js"];
-    NSString* donutPath = [[NSBundle mainBundle] pathForResource:@"donutLegend" ofType:@"js"];
-    NSString* donutInit = [NSString stringWithContentsOfFile:donutPath encoding:NSUTF8StringEncoding error:nil];
-    NSString* theme = @"";
+    
+    
     
     //
     //    NSString* xaxisPath = [[NSBundle mainBundle] pathForResource: @"xaxis" ofType: @"json"];
     //    NSString* xaxis = [NSString stringWithContentsOfFile: xaxisPath encoding: NSUTF8StringEncoding error:nil];
     //
-        NSString* dataPath = [[NSBundle mainBundle] pathForResource: @"data" ofType: @"json"];
-        NSString* json = [NSString stringWithContentsOfFile: dataPath encoding: NSUTF8StringEncoding error:nil];
+    
     //
     
-        NSString* baseline = [theme stringByAppendingFormat:donutInit, json];
+    
 //    NSString* donut = [theme stringByAppendingString:donutInit];
     
     
-    [self.webView stringByEvaluatingJavaScriptFromString:baseline];
+    
 
 }
 
-- (void)test {
-//    
-//    JSContext *context = [[JSContext alloc] init];
-//    [context evaluateScript:_htmlString];
-//    JSValue *function = context[@"greet"];
-//    JSValue* result = [function callWithArguments:@[@"World"]];
+//load viewOption
+- (void)loadViewOption {
     
+    //load donutLegend
+    [self loadDonutLegend];
+
+}
+
+
+
+- (void)loadBaseline {
+    NSString* baselinePath = [[NSBundle mainBundle] pathForResource:@"baseline" ofType:@"js"];
+    NSString* baselineInit = [NSString stringWithContentsOfFile:baselinePath encoding:NSUTF8StringEncoding error:nil];
+    
+    NSString* dataPath = [[NSBundle mainBundle] pathForResource: @"data" ofType: @"json"];
+    NSString* json = [NSString stringWithContentsOfFile: dataPath encoding: NSUTF8StringEncoding error:nil];
+    NSString* theme = @"";
+    NSString* baseline = [theme stringByAppendingFormat:baselineInit, json];
+    
+    [self.webView stringByEvaluatingJavaScriptFromString:baseline];
+}
+
+
+- (void)loadDonutLegend {
+    NSString* donutPath = [[NSBundle mainBundle] pathForResource:@"donutLegend" ofType:@"js"];
+    NSString* donutInit = [NSString stringWithContentsOfFile:donutPath encoding:NSUTF8StringEncoding error:nil];
+    NSString* dataPath = [[NSBundle mainBundle] pathForResource: @"data" ofType: @"json"];
+    NSString* json = [NSString stringWithContentsOfFile: dataPath encoding: NSUTF8StringEncoding error:nil];
+    NSString* theme = @"";
+    NSString* baseline = [theme stringByAppendingFormat:donutInit, json];
+    
+    [self.webView stringByEvaluatingJavaScriptFromString:baseline];
+}
+
+
+- (void)loadJSFiles {
+    [self loadJSLibFiles];
+    [self loadViewOption];
+}
+
+
+//use when method1
+- (void)loadDatas {
     [self.webView stringByEvaluatingJavaScriptFromString:@"plot([0,1,2,3,4,5,6,7,8,9])"];
 }
 
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     
+    //method 1
+    [self loadDatas];
     
-    [self test];
-//    [self loadWeb];
-    
-//    NSString* testPath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"js"];
-//    NSString* testInit = [NSString stringWithContentsOfFile:testPath encoding:NSUTF8StringEncoding error:nil];
-//    
-//    NSString* test = @"window.onload=function(){var chart = $('#container').highcharts();inCome = [0, 1, 2];chart.series[0].setData(inCome, true);}";
-    
-//    sleep(1);
-    
-//    NSString* theme = @"";
-//    NSString* donut = [theme stringByAppendingString:testInit];
-//    
-//    [self.webView stringByEvaluatingJavaScriptFromString:donut];
-    
-//    [self.webView stringByEvaluatingJavaScriptFromString:test];
-//    [self runJavaScript];
-    
-    
-    
-//    [self.donutWebView stringByEvaluatingJavaScriptFromString:donut];
+    //method 2
+    [self loadJSLibFiles];
+
 }
 
 @end
