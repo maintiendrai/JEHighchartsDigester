c5f2900f3ebfc5f461d1431bce97f8d9575d74dd
lilkr <maintiendrai@gmail.com>
2015-11-26 11:46:21 +0800

    add combination 2

--- JEHighchartsDigesterDemo/JEHighchartsDigesterDemo/JEHighchartsDigester/js/highcharts/viewOption/test.js
+++
@@ -1,8 +0,0 @@
-function plots(data) {
-  var chart = $('#container').highcharts();
-  inCome = [5, 35, 5, 12, 15, 20, 426, 660, 345, 626, 344, 13, 56];
-  chart.series[0].setData(inCome, true);
-    return 'Hello, ' + data;
-}
-
-function greet(name){ return 'Hello, ' + name; }
