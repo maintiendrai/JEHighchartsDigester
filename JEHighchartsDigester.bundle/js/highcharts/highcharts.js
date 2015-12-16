1cc95899b9b1593ae0778e779ddb624be136f688
lilkr <maintiendrai@gmail.com>
2015-12-16 17:44:35 +0800

    arrivedPlotFilled modify

--- JEHighchartsDigester.bundle/js/highcharts/highcharts.js
+++ JEHighchartsDigester.bundle/js/highcharts/highcharts.js
@@ -1262,7 +1262,7 @@
      *****************************************************************************/
     defaultOptions = {
         colors: ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c',
-                '#8085e9', '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'],
+                '#8085e9', '#f15c80', '#fcd653', '#2b908f', '#f45b5b', '#91e8e1'],
         symbols: ['circle', 'diamond', 'square', 'triangle', 'triangle-down'],
         lang: {
             loading: 'Loading...',
